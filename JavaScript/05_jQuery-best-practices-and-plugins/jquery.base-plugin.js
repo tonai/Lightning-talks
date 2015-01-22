ce888368db1bec6bc7350fc809bc0dc7193958fd
Tony Cabaye <tonai59+github@gmail.com>
2015-01-22 18:02:03 +0100

    Create jquery.base-plugin.js

---
+++ JavaScript/05_jQuery-best-practices-and-plugins/jquery.base-plugin.js
@@ -0,0 +1,56 @@
+(function($){
+  'use strict';
+  
+  /* Plugin default options. */
+  var pluginName = 'myPlugin';
+  var defaultOptions = {};
+  
+  /**
+   * Constructor.
+   */
+  function Plugin(element, options) {
+    // Merge specific and default options.
+    this.options = $.extend({}, defaultOptions, options);
+
+    // Initialize the main element.
+    this.$element = (element instanceof $)? element: $(element);
+
+    // Save the instance reference into the DOM element.
+    this.$element.data(pluginName, this);
+
+    // Object initialisation.
+    this.setup && this.setup();
+    this.bind  && this.bind();
+    this.init  && this.init();
+  };
+
+  /********** Start plugin specific code **********/
+
+  /**
+   * Setup plugin.
+   * e.g. Get DOM elements, setup data...
+   */
+  Plugin.prototype.setup = function() {};
+
+  /**
+   * Bind events.
+   */
+  Plugin.prototype.bind = function() {};
+
+  /**
+   * Initialize default plugin state.
+   */
+  Plugin.prototype.init = function() {};
+
+  /********** End plugin specific code **********/
+
+  /* Expose jQuery plugin. */
+  $.fn[pluginName] = function(options) {
+    return this.each(function() {
+      var $this = $(this);
+      if (!$this.data(pluginName)) {
+        new Plugin($this, options);
+      }
+    });
+  };
+})(jQuery);
